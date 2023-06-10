adcaa1086fa7f5e9461fe1813a37e73d15d42d15
Kunal Dhobe <88238035+Kunaldoe@users.noreply.github.com>
2023-06-10 17:50:02 +0530

    Time: 11 ms (9.86%), Space: 6 MB (46.01%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   int maxValue(int n, int index, int maxSum) {
+
+    int left = index;
+    int right = index;
+    int ans = 1;
+    maxSum = maxSum - n;
+
+    while (left > 0 || right < n - 1) {
+
+        int len = right - left + 1;
+
+        if (maxSum >= len) {
+            maxSum= maxSum - len;
+            ans++;
+            left = max(0, left - 1);
+            right = min(n - 1, right + 1);
+        }
+ 
+        else 
+        {
+            break;
+        }
+    }
+    ans += (maxSum / n);
+    return ans;
+    }
+};
